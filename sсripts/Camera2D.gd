68e5c6cd9130b8856d81afcf173f2b60f680cacd
fedirky <fedirky@gmail.com>
2024-08-08 19:12:48 +0300

    fixed camera limitations
    fixed a small bug with "slow" camera directions switching, now works perfect

--- sсripts/Camera2D.gd
+++ sсripts/Camera2D.gd
@@ -1,32 +1,40 @@
 extends Camera2D
 
-var screen_edge_threshold = 20
-var max_speed = 256
+var threshold_screen_edge = 20
+var speed_max = 256
+
+var left_limit
+var top_limit
+var right_limit
+var bottom_limit
 
 func _ready():
 	var limits = $"../TileMap".map_limits
-	limit_left   = limits[0]
-	limit_top    = limits[1]
-	limit_right  = limits[2]
-	limit_bottom = limits[3]
+	left_limit = limits[0]
+	top_limit = limits[1]
+	right_limit = limits[2]
+	bottom_limit = limits[3]
 
 func _process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 	var viewport_size = get_viewport().size
 	var move_vector = Vector2()
 
-	if mouse_pos.x < screen_edge_threshold:
-		move_vector.x = -lerp(0, max_speed, 1 - mouse_pos.x / screen_edge_threshold)
-	elif mouse_pos.x > viewport_size.x - screen_edge_threshold:
-		move_vector.x = lerp(0, max_speed, (mouse_pos.x - (viewport_size.x - screen_edge_threshold)) / screen_edge_threshold)
+	if mouse_pos.x < threshold_screen_edge:
+		move_vector.x = -lerp(0, speed_max, 1 - mouse_pos.x / threshold_screen_edge)
+	elif mouse_pos.x > viewport_size.x - threshold_screen_edge:
+		move_vector.x = lerp(0, speed_max, (mouse_pos.x - (viewport_size.x - threshold_screen_edge)) / threshold_screen_edge)
 
-	if mouse_pos.y < screen_edge_threshold:
-		move_vector.y = -lerp(0, max_speed, 1 - mouse_pos.y / screen_edge_threshold)
-	elif mouse_pos.y > viewport_size.y - screen_edge_threshold:
-		move_vector.y = lerp(0, max_speed, (mouse_pos.y - (viewport_size.y - screen_edge_threshold)) / screen_edge_threshold)
+	if mouse_pos.y < threshold_screen_edge:
+		move_vector.y = -lerp(0, speed_max, 1 - mouse_pos.y / threshold_screen_edge)
+	elif mouse_pos.y > viewport_size.y - threshold_screen_edge:
+		move_vector.y = lerp(0, speed_max, (mouse_pos.y - (viewport_size.y - threshold_screen_edge)) / threshold_screen_edge)
 
 	global_position += move_vector * delta
 
+	# Ensure camera stays within limits
+	global_position.x = clamp(global_position.x, left_limit, right_limit)
+	global_position.y = clamp(global_position.y, top_limit, bottom_limit)
 
 
 '''
